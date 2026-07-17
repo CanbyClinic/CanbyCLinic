81ea22db27faf347f5ceab52d9a80d755fbf7545
CanbyClinic <Info@canbycc.org>
2026-07-16 22:12:56 -0700

    V 4.3 Core Part 1

---
+++ v43/js/config-v43.js
@@ -0,0 +1,11 @@
+/* Secure services must be supplied by vendors covered by the clinic's HIPAA risk assessment and, when required, a signed Business Associate Agreement. */
+window.CANBY_CONFIG = {
+  donationCheckoutUrl: "",
+  portalLoginUrl: "",
+  portalSignupUrl: "",
+  portalResetUrl: "",
+  secureCallbackUrl: "",
+  secureIntakeUrl: "",
+  clinicEmail: "info@puravidacc.org",
+  volunteerFormEndpoint: ""
+};
